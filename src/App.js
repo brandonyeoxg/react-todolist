71cf16a00d784652a8cd3e89979cc0ca01c8a077
brandonyeoxg <brandonyeoxg@gmail.com>
2019-03-11 15:06:26 +0800

    Add todo list basic functionality - Add Todo - Delete Todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos';
+import AddTodo from './AddTodo';
 
 class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'play dragon age'},
+    ],
+    curId: 2
+  }
+
+  addTodo = (todo) => {
+    const id = this.state.curId + 1;
+    this.setState({curId:id});
+    todo.id = id;
+    const todoCopy = [...this.state.todos, todo];
+    this.setState({todos: todoCopy});
+  }
+
+  deleteTodo = (id) => {
+    // console.log(id);
+    const newTodoList = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+
+    this.setState({todos: newTodoList});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos deleteTodo={this.deleteTodo} todos={this.state.todos}/>
+        <AddTodo addTodo={this.addTodo} />
       </div>
     );
   }
