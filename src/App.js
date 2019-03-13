39a1786b9a71ab77c214e720253a999cffb219a1
brandonyeoxg <brandonyeoxg@gmail.com>
2019-03-13 12:32:40 +0800

    Start setup of the TodoList

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
 import React, { Component } from 'react';
-import Todos from './Todos';
-import AddTodo from './AddTodo';
-
+import {BrowserRouter} from 'react-router-dom';
 class App extends Component {
-  state = {
-    todos: [
-      {id: 1, content: 'buy some milk'},
-      {id: 2, content: 'play dragon age'},
-    ],
-    curId: 2
-  }
-
-  addTodo = (todo) => {
-    const id = this.state.curId + 1;
-    this.setState({curId:id});
-    todo.id = id;
-    const todoCopy = [...this.state.todos, todo];
-    this.setState({todos: todoCopy});
-  }
-
-  deleteTodo = (id) => {
-    // console.log(id);
-    const newTodoList = this.state.todos.filter(todo => {
-      return todo.id !== id;
-    });
-
-    this.setState({todos: newTodoList});
-  }
-
   render() {
     return (
+      <BrowserRouter>
       <div className="todo-app container">
-        <h1 className="center blue-text">Todo's</h1>
-        <Todos deleteTodo={this.deleteTodo} todos={this.state.todos}/>
-        <AddTodo addTodo={this.addTodo} />
+        <h1>Todo List</h1>
       </div>
+      </BrowserRouter>
     );
   }
 }
