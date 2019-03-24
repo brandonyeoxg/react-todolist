296b419360129a3de7c16680b4069c82a5b554d6
brandonyeoxg <brandonyeoxg@gmail.com>
2019-03-24 17:27:19 +0800

    Add tracking of user profile through the flags that is setup on the reactReduxFirebase enhancer in index.js

--- src/index.js
+++ src/index.js
@@ -17,8 +17,13 @@ const store = createStore(rootReducer,
   compose(
     applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
     reduxFirestore(firebase),
-    reactReduxFirebase(firebase, {attachAuthIsReady: true})
-  ));
+    reactReduxFirebase(firebase, {
+      useFirestoreForProfile: true, 
+      userProfile: 'users', 
+      attachAuthIsReady: true,
+    })
+  )
+);
 
 store.firebaseAuthIsReady.then(() => {
   ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
