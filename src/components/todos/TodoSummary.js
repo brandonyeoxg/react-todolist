70ed87153cbbddbbb8a467fa3ec4d4b4bfcb160f
brandonyeoxg <brandonyeoxg@gmail.com>
2019-03-25 16:52:11 +0800

    Reformat title to make it more appealing

--- src/components/todos/TodoSummary.js
+++ src/components/todos/TodoSummary.js
@@ -5,7 +5,7 @@ const TodoSummary = ({todo}) => {
   return (
     <div className="card z-depth-0 todo-summary">
       <div className="card-content grey-text text-darken-3">
-        <span className="card-title">{todo.title} - {todo.id}</span>
+        <span className="card-title">{todo.title}</span>
         <p>Posted by {todo.authorFirstName} {todo.authorLastName}</p>
         <p className="grey-text">{moment(todo.createdAt.toDate()).calendar()}</p>
       </div>
